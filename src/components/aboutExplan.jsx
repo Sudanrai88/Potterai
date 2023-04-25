e07e29aaa387b2b2ed8cfcb393dd31282a59c4b8
Sudanrai88 <101595305+Sudanrai88@users.noreply.github.com>
2023-04-25 17:22:45 +0100

    Update aboutExplan.jsx

--- src/components/aboutExplan.jsx
+++ src/components/aboutExplan.jsx
@@ -2,11 +2,39 @@ import React from 'react'
 
 function AboutExplan() {
   return (
-    <div className='mx-auto text-center py-24'>
-        <p className='text-2xl'>Sus.ceramics is a small pottery shop</p>
-        <p className='text-xl'> I am an independent beginner potterer who loves to create, design and produce unique peices of work. So creative, using it actually food taste better.</p>
+    <div className='mx-auto text-center py-24 max-w-5xl'>
+      <div>
+        <p className='text-3xl'>Sus.ceramics is a small pottery shop</p>
+        <p className='mt-5 text-2xl'> I am an independent beginner potterer who loves to create, design and produce unique peices of work. So creative, using it actually food taste better.</p>
+      </div>
+      <div className="container m-auto grid">
+        <div class="photoset-row cols-2">
+          <div>
+            <image>
+
+            </image>
+          </div>
+        </div>
+
+        <div class="photoset-row cols-4" style="margin-bottom: 5px; clear: left; display: block; overflow: hidden; height: 237.483px;">
+          <div class="photoset-cell">
+            <image>
+
+            </image>
+          </div>
+        </div>
+
+        <div class="photoset-row cols-4">
+          <div>
+            <image>
+
+            </image>
+          </div>
+        </div>
+
+      </div>
     </div>
-  )
+        )
 }
 
 export default AboutExplan
