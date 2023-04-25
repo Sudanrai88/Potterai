eff39010fed85629baa90b3705b2ac5d58491084
Sudanrai88 <101595305+Sudanrai88@users.noreply.github.com>
2023-04-25 20:27:22 +0100

    Fixed images on instagram, so it can build

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import InstagramImg from './InstagramImg';
-import IgImg1 from 'public/assets/IMG_5948.JPG'
-import IgImg2 from 'public/assets/IMG_5943.JPG'
-import IgImg3 from 'public/assets/IMG_5944.JPG'
-import IgImg4 from 'public/assets/IMG_5945.JPG'
-import IgImg5 from 'public/assets/IMG_5881.JPG'
-import IgImg6 from 'public/assets/IMG_5952.JPG'
-import IgImg7 from 'public/assets/IMG_6118.JPG'
-import IgImg8 from 'public/assets/IMG_6119.JPG'
 
 const Instagram = () => {
   return (
@@ -16,22 +8,22 @@ const Instagram = () => {
         <p className='pb-4'> @SusCeramics</p>
         <div>
           <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-2 p-4'>
-            <InstagramImg socialImg={IgImg1} />
-            <InstagramImg socialImg={IgImg2} />
-            <InstagramImg socialImg={IgImg3} />
+            <InstagramImg socialImg='/assets/IMG_5948.JPG' />
+            <InstagramImg socialImg={'/assets/IMG_5943.JPG'} />
+            <InstagramImg socialImg={'/assets/IMG_5944.JPG'} />
           </div>
           <div className='grid col-auto grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-2 p-4'>
             <div className='col-span-1 md:col-span-2 lg:col-span-4'>
-              <InstagramImg socialImg={IgImg4} />
+              <InstagramImg socialImg={'/assets/IMG_5945.JPG'} />
             </div>
             <div className='col-span-1 md:col-span-2 lg:col-span-4'>
-              <InstagramImg socialImg={IgImg8} />
+              <InstagramImg socialImg={'/assets/IMG_6119.JPG'} />
             </div>
           </div>
           <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-2 p-4'>
-            <InstagramImg socialImg={IgImg6} />
-            <InstagramImg socialImg={IgImg7} />
-            <InstagramImg socialImg={IgImg5} />
+            <InstagramImg socialImg={'/assets/IMG_5952.JPG'} />
+            <InstagramImg socialImg={'/assets/IMG_6118.JPG'} />
+            <InstagramImg socialImg={'/assets/IMG_5881.JPG'} />
           </div>
         
         </div>
