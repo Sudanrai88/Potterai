7d90e8f892557fa149ce1b8317445c8e1ad0df57
Sudanrai88 <sudanrai88@hotmail.com>
2023-06-02 12:14:38 +0100

    JPG -> jpg ...

--- src/components/AboutPage/imageByIndex.jsx
+++ src/components/AboutPage/imageByIndex.jsx
@@ -1,7 +1,7 @@
-import image1 from "src/assets/img1.JPG"
-import image2 from "src/assets/img2.JPG"
-import image3 from "src/assets/img3.JPG"
-import image4 from "src/assets/img4.JPG"
+import image1 from "src/assets/img1.jpg"
+import image2 from "src/assets/img2.jpg"
+import image3 from "src/assets/img3.jpg"
+import image4 from "src/assets/img4.jpg"
 
 export const images = [image1, image2, image3, image4]
 
