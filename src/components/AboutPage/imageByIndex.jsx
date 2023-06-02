80de0c4069925c3ed383f8c4a981a70587524d55
Sudanrai88 <sudanrai88@hotmail.com>
2023-06-02 11:29:34 +0100

    Moved assets/"anyImage" from public to src so netlify can find the module

--- src/components/AboutPage/imageByIndex.jsx
+++ src/components/AboutPage/imageByIndex.jsx
@@ -1,7 +1,7 @@
-import image1 from "/public/assets/IMG1.JPG"
-import image2 from "/public/assets/IMG2.JPG"
-import image3 from "/public/assets/IMG3.JPG"
-import image4 from "/public/assets/IMG4.JPG"
+import image1 from "/src/assets/IMG1.JPG"
+import image2 from "/src/assets/IMG2.JPG"
+import image3 from "/src/assets/IMG3.JPG"
+import image4 from "/src/assets/IMG4.JPG"
 
 export const images = [image1, image2, image3, image4]
 
